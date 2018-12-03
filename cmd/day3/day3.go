1fb8399b28c6f7761f4475f373854531577d86ed
Milan Pavlik <milanpavlik@improbable.io>
2018-12-03 20:10:52 +0000

    Solve day 3, part 2

--- cmd/day3/day3.go
+++ cmd/day3/day3.go
@@ -19,10 +19,10 @@ func (g *Grid) dimensions() (w int64, h int64) {
 	maxY := int64(0)
 
 	for _, claim := range g.claims {
-		if claim.left + claim.width > maxX {
+		if claim.left+claim.width > maxX {
 			maxX = claim.left + claim.width
 		}
-		if claim.top + claim.height > maxY {
+		if claim.top+claim.height > maxY {
 			maxY = claim.top + claim.height
 		}
 	}
@@ -30,18 +30,22 @@ func (g *Grid) dimensions() (w int64, h int64) {
 	return maxX, maxY
 }
 
-func (g *Grid) plot() [][]int {
+func (g *Grid) plot() [][][]*Claim {
 	width, height := g.dimensions()
 
-	index := make([][]int, height)
+	index := make([][][]*Claim, height)
 	for i := range index {
-		index[i] = make([]int, width)
+		row := make([][]*Claim, width)
+		for j := range row {
+			row[j] = make([]*Claim, 0)
+		}
+		index[i] = row
 	}
 
 	for _, claim := range g.claims {
-		for col := claim.top; col < claim.top + claim.height; col++ {
-			for row := claim.left; row < claim.left + claim.width; row++ {
-				index[col][row] += 1
+		for col := claim.top; col < claim.top+claim.height; col++ {
+			for row := claim.left; row < claim.left+claim.width; row++ {
+				index[col][row] = append(index[col][row], claim)
 			}
 		}
 	}
@@ -54,13 +58,31 @@ func (g *Grid) countOverlap(minClaim int) int {
 	plot := g.plot()
 	for i := range plot {
 		for j := range plot[i] {
-			if plot[i][j] >= minClaim {
+			if len(plot[i][j]) >= minClaim {
 				c += 1
 			}
 		}
 	}
 
 	return c
+}
+
+func (g *Grid) nonoverlapping() *Claim {
+	plot := g.plot()
+	for _, claim := range g.claims {
+		eligible := true
+		for col := claim.top; col < claim.top+claim.height && eligible; col++ {
+			for row := claim.left; row < claim.left+claim.width && eligible; row++ {
+				eligible = len(plot[col][row]) == 1 && eligible
+			}
+		}
+
+		if eligible {
+			return claim
+		}
+	}
+
+	return nil
 }
 
 type Claim struct {
@@ -129,5 +151,5 @@ func main() {
 
 	grid := &Grid{claims}
 	fmt.Println(fmt.Sprintf("Part one: %d", grid.countOverlap(2)))
+	fmt.Println(fmt.Sprintf("Part two: %s", grid.nonoverlapping().id))
 }
-
